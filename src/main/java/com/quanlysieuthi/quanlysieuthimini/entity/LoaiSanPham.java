4dbf9657a2060c42e2fae6311068adf6c9f23404
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2025-01-16 20:09:05 +0700

    push Entity

---
+++ src/main/java/com/quanlysieuthi/quanlysieuthimini/entity/LoaiSanPham.java
@@ -0,0 +1,7 @@
+package com.quanlysieuthi.quanlysieuthimini.entity;
+
+public class LoaiSanPham {
+    private String maLoai;
+    private String tenLoai;
+    private int soLuongToiDa;
+}
---
+++ src/main/java/com/quanlysieuthi/quanlysieuthimini/entity/NhaCungCap.java
@@ -0,0 +1,9 @@
+package com.quanlysieuthi.quanlysieuthimini.entity;
+
+public class NhaCungCap {
+    private String maNhaCungCap;
+    private String tenNhaCungCap;
+    private String soDienThoai;
+    private String email;
+    private String diaChi;
+}
